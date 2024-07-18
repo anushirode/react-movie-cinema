205c7435bb793ebdbe7ac4c0caf689e9754ce124
Atharva-77 <atharva.shirode99@gmail.com>
2024-07-18 05:18:21 -0400

    Add input, button field

--- ems-frontend/src/listEmployeeComponent.jsx
+++ ems-frontend/src/listEmployeeComponent.jsx
@@ -7,28 +7,11 @@ const GET_ALL_CINEMAS = "http://localhost:8084/cinema/allcinema";
 // const GET_ALL_CINEMAS = "http://localhost:8084/movies/all";
 
 function ListEmployeeComponent() {
-  const dummyData = [
-    {
-      id: 1,
-      firstname: "Anushka",
-      lastname: "Shirode",
-      email: "anushkashirode@gmail.com",
-    },
-    {
-      id: 2,
-      firstname: "Anu",
-      lastname: "Shi",
-      email: "anushi@gmail.com",
-    },
-    {
-      id: 3,
-      firstname: "Abc",
-      lastname: "Xyz",
-      email: "abcxyz@gmail.com",
-    },
-  ];
-
-  const [movieList, setMovieList] = useState([]);
+  const [cinemaHallDetails, setCinemaHallDetails] = useState([]);
+  const [newDetail, setNewDetail] = useState({});
+  const [cinema_name, setcinema_name] = useState({});
+  const [movie_name, setmovie_name] = useState({});
+  const [reload, setReload] = useState(false);
 
   // useEffect(()=>{listEmployees?.then((response) => {setEmployee(response.data);}).catch(error => console.error(error));},[])
   useEffect(() => {
@@ -36,21 +19,49 @@ function ListEmployeeComponent() {
       .get(GET_ALL_CINEMAS)
       .then((res) => {
         console.log("-->", res.data);
-        setMovieList(res.data);
-        // res.data.map((each_cinema, index) => {
-        //   // console.log(i.cinema_name)
-        //   each_cinema.movies.map((each_movie) => {
-        //     console.log(index, each_cinema.cinema_name, each_movie.movie_name);
-        //   });
-        // });
+        setCinemaHallDetails(res.data);
+        res.data.map((each_cinema, index) => {
+          // console.log(i.cinema_name)
+          each_cinema.movies.map((each_movie) => {
+            console.log(index, each_cinema.cinema_name, each_movie.movie_name);
+          });
+        });
       })
       .catch((err) => console.log("ERR", err));
     // console.log("VAL OF", listEmployees);
-  }, [setMovieList]);
+  }, [setCinemaHallDetails, reload]);
+
+  const onChange = (e) => {
+    console.log(e.target.name, e.target.value, Object.keys(newDetail).length);
+    if (e.target.name === "cinema_name") {
+      setcinema_name(e.target.value);
+    } else {
+      setmovie_name(e.target.value);
+    }
+    // setNewDetail({
+    //   ...newDetail,
+    //   [e.target.name]: e.target.value,
+    // });
+  };
+
+  const onClick = (e) => {
+    // console.log("===",movie_name);
+
+    const movies = [{ movie_name: movie_name }];
+    const final_data = { cinema_name, movies };
+    console.log(final_data);
+    axios
+      .post("http://localhost:8084/cinema/add", final_data)
+      .then((res) => {
+        console.log("Send data ", res);
+        setReload(!reload);
+      })
+      .catch((err) => console.log("ERR", err));
+  };
   return (
     <div>
       <h1>Cinema Hall Details</h1>
-     
+
       <div className="container">
         {/* <h2 className="text-center">List Employee</h2> */}
         <table className="table table-striped table-bordered">
@@ -61,7 +72,7 @@ function ListEmployeeComponent() {
             </tr>
           </thead>
           <tbody>
-            {movieList.map((each_cinema, index_cinema) =>
+            {cinemaHallDetails.map((each_cinema, index_cinema) =>
               each_cinema.movies.map(
                 (each_movie) => (
                   <tr
@@ -71,8 +82,8 @@ function ListEmployeeComponent() {
                       each_movie.movie_name
                     }
                   >
-                    <td>{each_cinema.cinema_name}</td>
-                    <td>{each_movie.movie_name}</td>
+                    <td>{each_cinema?.cinema_name}</td>
+                    <td>{each_movie?.movie_name}</td>
                   </tr>
                 )
                 // console.log(
@@ -84,9 +95,30 @@ function ListEmployeeComponent() {
             )}
           </tbody>
         </table>
-
-        
       </div>
+      {/* Add New Data */}
+      <div>
+        <div className="divElement">Cinema Hall Name</div>
+        <input
+          className="inputTag"
+          // type={type}
+          name="cinema_name"
+          value={cinema_name?.cinema_name}
+          onChange={onChange}
+          placeholder="Add New Cinema Hall"
+        />
+        <div className="divElement">Movie Name</div>
+        <input
+          className="inputTag"
+          // type={type}
+          name="movie_name"
+          value={movie_name?.movie_name}
+          onChange={onChange}
+          placeholder="Add New Cinema Hall"
+        />
+      </div>
+      {/* disabled={Object.keys(newDetail).length!=2} */}
+      <button onClick={onClick}>Add New Details</button>
     </div>
   );
 }
